f643ad4db685832e8f09dcf2b229483a046be8e3
jonathnawill <jonathanwilliam11@outlook.com>
2026-05-21 17:43:22 -0300

    feat: iniciando estrutura do projeto

---
+++ src/main/java/com/documentbatch/DocumentBatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.documentbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentBatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentBatchBackendApplication.class, args);
+	}
+
+}
